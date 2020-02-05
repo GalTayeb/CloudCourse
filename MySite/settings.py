62a85423ac7a2615c9d516484ff9cabc9f0c3e20
AmnonBZ <amnon.bn1992@gmail.com>
2020-02-05 21:21:19 +0200

    add 0.0 to allowed hosts

--- MySite/settings.py
+++ MySite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1usbgguttbt__or!)7hey*=a$_-g%s-cz0l)9r6c0!u(=xkcn6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0/0"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
