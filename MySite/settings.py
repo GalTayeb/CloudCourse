b7b84c8786b6ef4d5392a5d1e4be66a7daf625e4
AmnonBZ <amnon.bn1992@gmail.com>
2020-02-05 21:18:55 +0200

    add 0.0 to allowed hosts

--- MySite/settings.py
+++ MySite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1usbgguttbt__or!)7hey*=a$_-g%s-cz0l)9r6c0!u(=xkcn6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0/0"]
 
 
 # Application definition
